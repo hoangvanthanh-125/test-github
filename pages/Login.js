21b274e350c1222a1072ab52aefb9a8f1afd98a1
thanh <thanhthanh125521@gmail.com>
2021-11-14 22:09:16 +0700

    xu li

--- pages/Login.js
+++ pages/Login.js
@@ -3,11 +3,7 @@ import React from 'react';
 function Login(props) {
   return (
     <div>
-<<<<<<< HEAD
       Login3
-=======
-      Login2
->>>>>>> 23285e888d8265a7d3413e38de264102550d0057
     </div>
   );
 }
